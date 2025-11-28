408318244c4f261ed92200e07bf0c0b933844ce4
Danelito81 <his_81@hotmail.com>
2025-11-28 17:58:30 +0300

    Delete Package.swift

--- Package.swift
+++
@@ -1,9 +0,0 @@
-// swift-tools-version:5.9
-import PackageDescription
-
-let package = Package(
-    name: "TryggSamtalApp",
-    platforms: [.iOS(.v15)],
-    products: [.library(name: "TryggSamtalApp", targets: ["TryggSamtalApp"])],
-    targets: [.target(name: "TryggSamtalApp", path: ".")]
-)
