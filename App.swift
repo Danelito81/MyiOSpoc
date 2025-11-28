d3a29dbec6132823f206ccd18260b27002997458
Danelito81 <his_81@hotmail.com>
2025-11-28 18:04:16 +0300

    Create App.swift

---
+++ App.swift
@@ -0,0 +1,9 @@
+@main
+import SwiftUI
+struct ContentView: View {
+    var body: some View {
+        Text("TryggSamtal TestFlight Works! 🎉")
+            .font(.largeTitle)
+            .padding()
+    }
+}
