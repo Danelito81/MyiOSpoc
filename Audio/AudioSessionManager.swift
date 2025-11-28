fb2628541cd6c66390b2d5c7ba4dc134f756989f
Danelito81 <his_81@hotmail.com>
2025-11-28 17:15:58 +0300

    Delete Audio directory

--- Audio/AudioSessionManager.swift
+++
@@ -1,8 +0,0 @@
-import AVFoundation
-final class AudioSessionManager {
-    func configureForMonitoring() throws {
-        let s = AVAudioSession.sharedInstance()
-        try s.setCategory(.playAndRecord, mode: .voiceChat, options: [.defaultToSpeaker, .allowBluetoothA2DP, .mixWithOthers])
-        try s.setActive(true, options: .notifyOthersOnDeactivation)
-    }
-}
