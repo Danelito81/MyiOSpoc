123ba158bdf392b301a4f62c8e4b0795b9af18f0
Danelito81 <his_81@hotmail.com>
2025-11-28 21:38:28 +0300

    Create App.swift

---
+++ Sources/TryggSamtalApp/App.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct TryggSamtalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("TryggSamtal in TestFlight! IT WORKS! 🎉")
+                .font(.largeTitle)
+                .padding()
+        }
+    }
+}
