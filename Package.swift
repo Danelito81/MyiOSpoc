61805d8f36532eef2489b9aec0a8781a3de1aacb
Danelito81 <his_81@hotmail.com>
2025-11-28 19:25:40 +0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,9 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "TryggSamtalApp",
+    platforms: [.iOS(.v15)],
+    products: [.executable(name: "TryggSamtalApp", targets: ["TryggSamtalApp"])],
+    targets: [.target(name: "TryggSamtalApp", path: ".")]
+)
