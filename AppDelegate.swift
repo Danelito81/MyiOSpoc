1dbc2ae66fa742f56b55576ff59268725915d41a
Danelito81 <his_81@hotmail.com>
2025-11-28 16:09:30 +0300

    Create AppDelegate.swift

---
+++ AppDelegate.swift
@@ -0,0 +1,14 @@
+@main
+struct TryggSamtalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("TryggSamtal POC Running!")
+    }
+}
